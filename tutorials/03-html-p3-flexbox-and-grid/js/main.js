8aac7d372464227a21b3bc80be9b6acda9040d8b
Eniugnil <darknightx7x@gmail.com>
2026-02-27 22:29:13 -0800

    Create tutorial 9

---
+++ tutorials/03-html-p3-flexbox-and-grid/js/main.js
@@ -0,0 +1,9 @@
+// console.log("asd");
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  console.log("asd");
+  menu.classList.toggle("active");
+});
