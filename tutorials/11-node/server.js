ade750d735e44971c7356bc155b92a67569dcda3
Eniugnil <darknightx7x@gmail.com>
2026-05-17 21:16:51 -0700

    Add tutorial 11

---
+++ tutorials/11-node/server.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("My socket server is running");
+
+let socket = require('socket.io');
+
+let io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log("new connection: " + socket.id);    
+
+    socket.on('mouse', mouseMessage);
+
+    function mouseMessage(data) {
+        socket.broadcast.emit('mouse', data);
+    }
+}
